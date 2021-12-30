0a6c3000ffdb0427cca6c3e4baaf919badff3043
Brandon Fergerson <brandon.fergerson@codebrig.com>
2021-12-30 13:05:32 -0500

    Update ProcessorAddress.kt

--- src/jvmMain/kotlin/spp/protocol/processor/ProcessorAddress.kt
+++ src/jvmMain/kotlin/spp/protocol/processor/ProcessorAddress.kt
@@ -4,6 +4,7 @@ enum class ProcessorAddress(val address: String) {
     LOGGING_PROCESSOR("sw.processor.logging"),
     LIVE_VIEW_PROCESSOR("sw.processor.live-view"),
     LIVE_INSTRUMENT_PROCESSOR("sw.processor.live-instrument"),
+    LIVE_AUDIT_PROCESSOR("sw.processor.live-audit"),
     BREAKPOINT_HIT("spp.provider.status.breakpoint-hit"),
     LOG_HIT("spp.provider.status.log-hit"),
     VIEW_SUBSCRIPTION_EVENT("spp.provider.status.view-subscription-event"),
