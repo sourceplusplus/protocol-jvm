3cc8456973ceae750df6cc8603011490c84f26d1
Brandon Fergerson <bfergerson@apache.org>
2022-01-03 22:01:49 -0500

    add live span

---
+++ src/commonMain/kotlin/spp.protocol/instrument/span/LiveSpan.kt
@@ -0,0 +1,34 @@
+package spp.protocol.instrument.span
+
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
+import spp.protocol.instrument.InstrumentThrottle
+import spp.protocol.instrument.LiveInstrument
+import spp.protocol.instrument.LiveInstrumentType
+import spp.protocol.instrument.LiveSourceLocation
+
+/**
+ * A live span represents a single unit of work in a program.
+ *
+ * @author [Brandon Fergerson](mailto:bfergerson@apache.org)
+ */
+@Serializable
+data class LiveSpan(
+    override val location: LiveSourceLocation,
+    override val condition: String? = null,
+    override val expiresAt: Long? = null,
+    override val hitLimit: Int = -1,
+    override val id: String? = null,
+    override val applyImmediately: Boolean = false,
+    override val applied: Boolean = false,
+    override val pending: Boolean = false,
+    override val throttle: InstrumentThrottle? = null,
+    override val meta: Map<String, @Contextual Any> = emptyMap()
+) : LiveInstrument() {
+    override val type: LiveInstrumentType = LiveInstrumentType.SPAN
+
+    /**
+     * Specify explicitly so Kotlin doesn't override.
+     */
+    override fun hashCode(): Int = super.hashCode()
+}
