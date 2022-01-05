e3313a1f65274d825589f9d3ab16b123adb26511
Brandon Fergerson <bfergerson@apache.org>
2022-01-04 23:34:07 -0500

    add name

--- src/commonMain/kotlin/spp.protocol/instrument/span/LiveSpan.kt
+++ src/commonMain/kotlin/spp.protocol/instrument/span/LiveSpan.kt
@@ -14,6 +14,7 @@ import spp.protocol.instrument.LiveSourceLocation
  */
 @Serializable
 data class LiveSpan(
+    val name: String,
     override val location: LiveSourceLocation,
     override val condition: String? = null,
     override val expiresAt: Long? = null,
