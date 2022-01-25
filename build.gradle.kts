e7cc1a4eaa88ab237656d1ad40a62387f2ad53a4
Brandon Fergerson <brandon.fergerson@codebrig.com>
2022-01-25 11:10:22 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,9 +49,8 @@ kotlin {
         val jvmMain by getting {
             dependencies {
                 implementation("io.vertx:vertx-core:$vertxVersion")
-                implementation("io.vertx:vertx-codegen:4.2.4")
+                implementation("io.vertx:vertx-codegen:$vertxVersion")
                 implementation("io.vertx:vertx-tcp-eventbus-bridge:$vertxVersion")
-                //implementation(files(".ext/vertx-service-discovery-4.0.3-SNAPSHOT.jar"))
                 implementation("io.vertx:vertx-service-proxy:4.1.5")
                 implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
                 implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:$jacksonVersion")
