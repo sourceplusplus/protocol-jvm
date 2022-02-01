86d7c69607fda8db7f3455d8a210e8af5771a5f8
Brandon Fergerson <bfergerson@apache.org>
2022-02-01 11:50:13 -0500

    refactor

--- src/commonMain/kotlin/spp.protocol/platform/PlatformAddress.kt
+++ src/commonMain/kotlin/spp.protocol/platform/PlatformAddress.kt
@@ -23,21 +23,8 @@ enum class PlatformAddress(val address: String) {
     PROBE_CONNECTED("spp.platform.status.probe-connected"),
     PROBE_DISCONNECTED("spp.platform.status.probe-disconnected"),
 
-    LIVE_BREAKPOINT_REMOVED("spp.platform.status.live-breakpoint-removed"),
-    LIVE_BREAKPOINT_APPLIED("spp.platform.status.live-breakpoint-applied"),
-    LIVE_BREAKPOINTS("spp.platform.status.live-breakpoints"),
-
-    LIVE_LOG_REMOVED("spp.platform.status.live-log-removed"),
-    LIVE_LOG_APPLIED("spp.platform.status.live-log-applied"),
-    LIVE_LOGS("spp.platform.status.live-logs"),
-
-    LIVE_METER_REMOVED("spp.platform.status.live-meter-removed"),
-    LIVE_METER_APPLIED("spp.platform.status.live-meter-applied"),
-    LIVE_METERS("spp.platform.status.live-meters"),
-
-    LIVE_SPAN_REMOVED("spp.platform.status.live-span-removed"),
-    LIVE_SPAN_APPLIED("spp.platform.status.live-span-applied"),
-    LIVE_SPANS("spp.platform.status.live-spans"),
+    LIVE_INSTRUMENT_REMOVED("spp.platform.status.live-instrument-removed"),
+    LIVE_INSTRUMENT_APPLIED("spp.platform.status.live-instrument-applied"),
 
     MARKER_CONNECTED("spp.status.marker-connected"),
     MARKER_DISCONNECTED("spp.platform.status.marker-disconnected"),
