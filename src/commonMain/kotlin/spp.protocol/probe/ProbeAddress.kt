c6f138312820e26d16f95244e20f5518ee6bfd40
Brandon Fergerson <bfergerson@apache.org>
2022-02-01 21:22:38 -0500

    refactor

--- src/commonMain/kotlin/spp.protocol/probe/ProbeAddress.kt
+++ src/commonMain/kotlin/spp.protocol/probe/ProbeAddress.kt
@@ -19,8 +19,5 @@ package spp.protocol.probe
 
 enum class ProbeAddress(val address: String) {
     REMOTE_REGISTERED("spp.probe.status.remote-registered"),
-    LIVE_BREAKPOINT_REMOTE("spp.probe.command.live-breakpoint-remote"),
-    LIVE_LOG_REMOTE("spp.probe.command.live-log-remote"),
-    LIVE_METER_REMOTE("spp.probe.command.live-meter-remote"),
-    LIVE_SPAN_REMOTE("spp.probe.command.live-span-remote");
+    LIVE_INSTRUMENT_REMOTE("spp.probe.command.live-instrument-remote")
 }
