591644124b1c84988b707efdf934f31731e4f30d
Brandon Fergerson <bfergerson@apache.org>
2022-03-19 09:02:12 +0100

    add CommandType and SHOW_QUICK_STATS command

--- src/commonMain/kotlin/spp.protocol/platform/auth/RolePermission.kt
+++ src/commonMain/kotlin/spp.protocol/platform/auth/RolePermission.kt
@@ -17,58 +17,62 @@
  */
 package spp.protocol.platform.auth
 
-enum class RolePermission(val manager: Boolean = true) {
-    RESET(true),
+import spp.protocol.platform.auth.CommandType.*
+
+enum class RolePermission(val manager: Boolean, val commandType: CommandType) {
+    RESET(true, LIVE_SERVICE),
 
     //devs
-    ADD_DEVELOPER(true),
-    REMOVE_DEVELOPER(true),
-    GET_DEVELOPERS(true),
-    REFRESH_DEVELOPER_TOKEN(true),
+    ADD_DEVELOPER(true, LIVE_SERVICE),
+    REMOVE_DEVELOPER(true, LIVE_SERVICE),
+    GET_DEVELOPERS(true, LIVE_SERVICE),
+    REFRESH_DEVELOPER_TOKEN(true, LIVE_SERVICE),
 
     //roles
-    ADD_ROLE(true),
-    REMOVE_ROLE(true),
-    GET_ROLES(true),
-    GET_DEVELOPER_ROLES(true),
-    ADD_DEVELOPER_ROLE(true),
-    REMOVE_DEVELOPER_ROLE(true),
+    ADD_ROLE(true, LIVE_SERVICE),
+    REMOVE_ROLE(true, LIVE_SERVICE),
+    GET_ROLES(true, LIVE_SERVICE),
+    GET_DEVELOPER_ROLES(true, LIVE_SERVICE),
+    ADD_DEVELOPER_ROLE(true, LIVE_SERVICE),
+    REMOVE_DEVELOPER_ROLE(true, LIVE_SERVICE),
 
     //permissions
-    GET_DEVELOPER_PERMISSIONS(true),
-    GET_ROLE_PERMISSIONS(true),
-    ADD_ROLE_PERMISSION(true),
-    REMOVE_ROLE_PERMISSION(true),
+    GET_DEVELOPER_PERMISSIONS(true, LIVE_SERVICE),
+    GET_ROLE_PERMISSIONS(true, LIVE_SERVICE),
+    ADD_ROLE_PERMISSION(true, LIVE_SERVICE),
+    REMOVE_ROLE_PERMISSION(true, LIVE_SERVICE),
 
     //instrument access
-    GET_ACCESS_PERMISSIONS(true),
-    GET_DATA_REDACTIONS(true),
-    ADD_DATA_REDACTION(true),
-    REMOVE_DATA_REDACTION(true),
-    ADD_ACCESS_PERMISSION(true),
-    REMOVE_ACCESS_PERMISSION(true),
+    GET_ACCESS_PERMISSIONS(true, LIVE_SERVICE),
+    GET_DATA_REDACTIONS(true, LIVE_SERVICE),
+    ADD_DATA_REDACTION(true, LIVE_SERVICE),
+    REMOVE_DATA_REDACTION(true, LIVE_SERVICE),
+    ADD_ACCESS_PERMISSION(true, LIVE_SERVICE),
+    REMOVE_ACCESS_PERMISSION(true, LIVE_SERVICE),
 
     //instruments
-    ADD_LIVE_BREAKPOINT(false),
-    ADD_LIVE_LOG(false),
-    ADD_LIVE_METER(false),
-    ADD_LIVE_SPAN(false),
-    GET_LIVE_INSTRUMENTS(false),
-    GET_LIVE_BREAKPOINTS(false),
-    GET_LIVE_LOGS(false),
-    GET_LIVE_METERS(false),
-    GET_LIVE_SPANS(false),
-    REMOVE_LIVE_INSTRUMENT(false),
-    CLEAR_ALL_LIVE_INSTRUMENTS(false),
+    ADD_LIVE_BREAKPOINT(false, LIVE_INSTRUMENT),
+    ADD_LIVE_LOG(false, LIVE_INSTRUMENT),
+    ADD_LIVE_METER(false, LIVE_INSTRUMENT),
+    ADD_LIVE_SPAN(false, LIVE_INSTRUMENT),
+    GET_LIVE_INSTRUMENTS(false, LIVE_INSTRUMENT),
+    GET_LIVE_BREAKPOINTS(false, LIVE_INSTRUMENT),
+    GET_LIVE_LOGS(false, LIVE_INSTRUMENT),
+    GET_LIVE_METERS(false, LIVE_INSTRUMENT),
+    GET_LIVE_SPANS(false, LIVE_INSTRUMENT),
+    REMOVE_LIVE_INSTRUMENT(false, LIVE_INSTRUMENT),
+    CLEAR_ALL_LIVE_INSTRUMENTS(false, LIVE_INSTRUMENT),
 
     //views
-    ADD_LIVE_VIEW_SUBSCRIPTION(false),
-    REMOVE_LIVE_VIEW_SUBSCRIPTION(false),
-    GET_LIVE_VIEW_SUBSCRIPTIONS(false),
-//    VIEW_OVERVIEW(false),
-    VIEW_ACTIVITY(false),
-    VIEW_TRACES(false),
-    VIEW_LOGS(false);
+    ADD_LIVE_VIEW_SUBSCRIPTION(false, LIVE_VIEW),
+    REMOVE_LIVE_VIEW_SUBSCRIPTION(false, LIVE_VIEW),
+    GET_LIVE_VIEW_SUBSCRIPTIONS(false, LIVE_VIEW),
+
+//    VIEW_OVERVIEW(false, LIVE_VIEW),
+    VIEW_ACTIVITY(false, LIVE_VIEW),
+    VIEW_TRACES(false, LIVE_VIEW),
+    VIEW_LOGS(false, LIVE_VIEW),
+    SHOW_QUICK_STATS(false, LIVE_VIEW);
 
     companion object {
         fun fromString(s: String): RolePermission? {
